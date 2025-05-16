027bc05b2332ea2e8cb9d4a2251b6b248b50125d
NickyLarson58 <nickylarson58@gmail.com>
2025-05-16 13:24:25 +0200

    Maj Beug Adresse cartographie

--- src/main/java/src/controller/CartographieController.java
+++ src/main/java/src/controller/CartographieController.java
@@ -119,12 +119,15 @@ public class CartographieController {
 
     @PutMapping("/api/signalements/{id}")
     @ResponseBody
-    public ResponseEntity<?> updateSignalement(@PathVariable Long id, @RequestParam(required = false) String description, @RequestParam(value = "files", required = false) MultipartFile[] files) {
+    public ResponseEntity<?> updateSignalement(@PathVariable Long id, @RequestParam(required = false) String description, @RequestParam(required = false) String adresse, @RequestParam(value = "files", required = false) MultipartFile[] files) {
         try {
             Signalement signalement = signalementRepository.findById(id)
                     .orElseThrow(() -> new RuntimeException("Le signalement demandé n'existe pas ou a été supprimé."));
             if (description != null) {
                 signalement.setDescription(description);
+            }
+            if (adresse != null) {
+                signalement.setAdresse(adresse);
             }
             if (files != null && files.length > 0) {
                 for (MultipartFile file : files) {
