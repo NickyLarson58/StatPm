9aa03ff8e2da51270701e35b08989b5b8c90103b
NickyLarson58 <nickylarson58@gmail.com>
2025-05-21 14:58:47 +0200

    Maj consulter liste-commercants

--- src/main/java/src/controller/CommercantController.java
+++ src/main/java/src/controller/CommercantController.java
@@ -3,11 +3,48 @@ package src.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import src.model.Commercant;
+import src.repository.CommercantRepository;
+
 @Controller
 public class CommercantController {
+
+    @Autowired
+    private CommercantRepository commercantRepository;
 
     @GetMapping("/AccueilListeCommercant")
     public String travaux() {
         return "AccueilListeCommercant";
+    }
+
+    @GetMapping("/consulter-commerce/{id}")
+    public String consulterCommerce(@PathVariable Long id, Model model) {
+        Commercant commercant = commercantRepository.findById(id).orElse(null);
+        model.addAttribute("commercant", commercant);
+        return "consulter-commerce";
+    }
+
+    @GetMapping("/modifier-commerce/{id}")
+    public String afficherModifierCommerce(@PathVariable Long id, Model model) {
+        Commercant commercant = commercantRepository.findById(id).orElse(null);
+        model.addAttribute("commercant", commercant);
+        return "declarer-commerce";
+    }
+
+    @PostMapping("/modifier-commerce/{id}")
+    public String modifierCommerce(@PathVariable Long id, Commercant commercant) {
+        commercant.setId(id);
+        commercantRepository.save(commercant);
+        return "redirect:/liste-commercants";
+    }
+
+    @GetMapping("/supprimer-commerce/{id}")
+    public String supprimerCommerce(@PathVariable Long id) {
+        commercantRepository.deleteById(id);
+        return "redirect:/liste-commercants";
     }
 }
